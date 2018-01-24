b8f390ca39f127ebc2ce62c8a8484fac6b800c78
hkoscielski <huberto1010@op.pl>
2018-01-24 22:38:03 +0100

    subjects in recycler view

--- app/src/main/java/com/example/hubson/systemdyplomant/view/subjects/SubjectListActivity.java
+++ app/src/main/java/com/example/hubson/systemdyplomant/view/subjects/SubjectListActivity.java
@@ -3,6 +3,7 @@ package com.example.hubson.systemdyplomant.view.subjects;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,17 +13,17 @@ import com.example.hubson.systemdyplomant.R;
 import com.example.hubson.systemdyplomant.viewmodel.SubjectListViewModel;
 
 public class SubjectListActivity extends AppCompatActivity {
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
-        textView = (TextView) findViewById(R.id.text_test);
+        RecyclerView subjectRecyclerView = (RecyclerView) findViewById(R.id.subject_list);
+        subjectRecyclerView.setHasFixedSize(true);
         SubjectListViewModel subjectListViewModel = ViewModelProviders.of(this).get(SubjectListViewModel.class);
         subjectListViewModel.getSubjects().observe(this, subjects -> {
             if(subjects != null && subjects.data != null)
-                textView.setText("Tematów: " + subjects.data.size() + "\n\n" + subjects.data.toString());
+                subjectRecyclerView.setAdapter(new SubjectListAdapter(subjects.data));
         });
     }
 
---
+++ app/src/main/java/com/example/hubson/systemdyplomant/view/subjects/SubjectListAdapter.java
@@ -0,0 +1,55 @@
+package com.example.hubson.systemdyplomant.view.subjects;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hubson.systemdyplomant.R;
+import com.example.hubson.systemdyplomant.repository.local.entity.Subject;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.ViewHolder> {
+    private List<Subject> subjects;
+
+    public SubjectListAdapter(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public SubjectListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.subject_list_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SubjectListAdapter.ViewHolder holder, int position) {
+        Subject subject = subjects.get(position);
+        holder.textSubject.setText(subject.getSubjectPl());
+        holder.textTakenUp.setText(subject.getTakenUp() + "/" + subject.getLimit());
+    }
+
+    @Override
+    public int getItemCount() {
+        return subjects.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.text_subject) TextView textSubject;
+        @BindView(R.id.text_supervisor) TextView textSupervisor;
+        @BindView(R.id.text_subject_status) TextView textSubjectStatus;
+        @BindView(R.id.text_taken_up) TextView textTakenUp;
+        @BindView(R.id.text_creators) TextView textCreators;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
